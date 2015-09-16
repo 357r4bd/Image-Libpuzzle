8c465f2d6dc6c164c1dc2edab3c9e1bdbe26c221
B. Estrade <estrabd@gmail.com>
2015-09-16 21:05:53 +0000

    modifying test

--- test.pl
+++ test.pl
@@ -8,22 +8,26 @@ use Data::Dumper ();
 
 # initial synopsis
 
+my $pic1 = q{pics/luxmarket_tshirt01.jpg};
+my $pic2 = q{pics/luxmarket_tshirt01_sal.jpg};
+
 my $p1 = Image::Libpuzzle->new; #( lambdas => 11, p_ratio => 2.0 );  # TODO, process these params
 my $p2 = Image::Libpuzzle->new; #( lambdas => 11, p_ratio => 2.0 );  # TODO, process these params
 
-my $sig1 = $p1->get_signature_from_file('./test.jpg');
-my $sig2 = $p2->get_signature_from_file('./test.jpg');
+my $sig1 = $p1->get_signature_from_file($pic1);
+my $sig2 = $p2->get_signature_from_file($pic2);
 
 foreach my $i ( 11, 9, 7, 5 ) {
   foreach my $j ( 2.0, 1.0, 0.5 ) {
     print "Lambda: $i, p ratio: $j\n";
     $p1->puzzle_set_lambdas($i);
     $p1->puzzle_set_p_ratio($j);
-    $sig1 = $p1->get_signature_from_file('./test.jpg');
+    $sig1 = $p1->get_signature_from_file($pic1);
     $p2->puzzle_set_lambdas($i);
     $p2->puzzle_set_p_ratio($j);
-    $sig2 = $p2->get_signature_from_file('./test.jpg');
-    print unpack("C*", $sig1);
+    $sig2 = $p2->get_signature_from_file($pic2);
+    print unpack("C*", $sig1), "\n";
+    print unpack("C*", $sig2);
     printf("\nEuclidean length: %f",$p1->puzzle_vector_euclidean_length);
     printf("\nDiff with \$p2: %f", $p1->puzzle_vector_normalized_distance($p2));
     print "\n\n";
@@ -31,8 +35,6 @@ foreach my $i ( 11, 9, 7, 5 ) {
 }
 
 my $cvec = $p1->get_cvec; # reference to Image::Libpuzzle::Cvec
-
-print Dump($cvec);
 
 __END__
 
